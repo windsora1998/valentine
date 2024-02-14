940aee59b819d10e6198d58050dcd863b6053fb9
windsora1998 <windsora1998@gmail.com>
2024-02-14 19:24:10 +0700

    phong update

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <ValentineCard/>
+</template>
+
+<script>
+import ValentineCard from './components/ValentineCard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ValentineCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+
+body {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ff758f;
+}
+</style>
