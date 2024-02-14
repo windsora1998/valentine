246af870747fab2bf5c806078ba44f078e04726a
windsora1998 <windsora1998@gmail.com>
2024-02-14 19:32:03 +0700

    phong update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
   ? '/valentine/' // Thay tên repository của các bạn vào đây nhé
   : '/'
